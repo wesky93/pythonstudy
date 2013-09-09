00b75131b919b59fbadffbe31e2e042b09738efc
wesky93 <wesky93@gmail.com>
2013-09-09 13:53:03 +0900

    모듈내 함수 추가
    다양한 실험을 위해 모듈내에 함수를 추가함

--- test/import/minicounter.py
+++ test/import/minicounter.py
@@ -8,12 +8,14 @@ def minicount():
 		a = float(input("첫번째 숫자를 입력하세요"))
 		b = float(input("두번째 숫자를 입력하세요"))
 		print ("각각 입력된수는",a,b,"입니다")
-		c = float(input("더하기는 1을 빼기는 2를 눌러주세요"))
+		c = float(input("더하기는 ",1,"빼기는",2,"를 입력하세요"))
 		if c == 1 :
-			print ("값은",a+b)
+			print ("정답은", a+b)
 		elif c == 2:
-			print ("값은",a-b)
+			print ("정답은", a-b)
 		else:
 			print ("숫자 1,2가 아닙니다.")
 
 
+def h2print():
+	print ("성공적으로 함수를 호출함")
