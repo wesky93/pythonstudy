521cf918f7dd0df6e5e23351b7777a25877fb1c8
wesky93 <wesky93@gmail.com>
2013-09-09 20:59:53 +0900

    미니계산기 개발 안료!!
    미니계산기 계발완료후 개발가지에 병합

--- 미니계산기/minicounter_main.py
+++ 미니계산기/minicounter_main.py
@@ -5,17 +5,23 @@
 runcheck = 0	#이전 계산값을 다음계산에 사용여부 판단함수 
 				#0이면 새로운 계산을 1이면 재사용을 의미
 
-num_a = int()	#계산기에 넣을 첫번째 숫자
-num_b = int()	#계산기에 넣을 두번째 숫자
+num_a = int()	#계산기에 넣을 첫번째 숫자는 정수형 선언
+num_b = int()	#계산기에 넣을 두번째 숫자는 정수형 선언
+
+#m_count모듈 및 내부함수 모조리 등록
+from m_count import *
+
+
 
 while 1:
     if runcheck==0:
         num_a = fnum()
         num_b = snum()
-        askcount(num_a,num_b)
+        num_a = askcount(num_a,num_b)
+        runcheck = 1
     elif runcheck==1:
-        resetcheck()
+        runcheck, num_b = resetcheck()    
         if runcheck == 0:
             pass
         elif runcheck == 1:
-            askcount(num_a,num_b)
+            num_a = askcount(num_a,num_b)
