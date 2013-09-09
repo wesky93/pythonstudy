ec6b26ef95d9e4d15a7760dabed3d11fa1caf15a
wesky93 <wesky93@gmail.com>
2013-09-09 13:53:58 +0900

    모듈내 함수를 모두 등록하는 실험
    모듈,함수()가 아닌 바로 함수를 호출할수 있는 import실험

---
+++ test/import/t_import2.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+
+#같은 폴더에 minicounter 파일이 있다
+#importtest가 모듈호출후 일일이 모듈.함수()로 함수호출을 했다면
+#이번엔 모듈내부의 모든 함수를 등록하여 바로 함수호출하는것 테스트
+
+from minicounter import *
+minicount()
+h2print()
