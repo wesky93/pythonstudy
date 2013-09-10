9532d947bcb5decc834ae7ef2f4968c8ca7f6c98
wesky93 <wesky93@gmail.com>
2013-09-10 20:59:37 +0900

    오타수정
    오타수

--- test/class/class-housepark.py
+++ test/class/class-housepark.py
@@ -2,11 +2,10 @@
 
 class housepark:
 	lastname = "박"
-	def setname(slef):
+	def setname(self):
 		name = input("이름을 입력하세요")
 		self.fullname = self.lastname + name
 
 	def travel(self):
 		where = input("여행하고 싶은곳은 어디인가요")
 		print("%s, %s 여행을 가고싶다." %(self.fullname, where))
-		
