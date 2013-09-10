8092e30114cb92b033d5aa427749fa432c913456
wesky93 <wesky93@gmail.com>
2013-09-10 20:40:07 +0900

    class 스터디
    #10 클래스로 사친연산 실습
    #11 클래스로 계산기 만들때 참고할 자료

---
+++ test/class/class-abc.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class fourcal:
+	def setdata(self, first, second):
+		self.first = first
+		self.second = second
+
+	def sum(self):
+		result = self.first + self.second
+		return result
+
+	def mul(self):
+		result = self.first * self.second
+		return result
+
+	def sub(self):
+		result = self.first - self.second
+		return result
+
+	def div(self):
+		result = self.first / self.second
+		return result
+
