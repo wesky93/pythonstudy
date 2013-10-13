8669c7144cba128cdbd8027bb6d543769542b61c
wesky93 <wesky93@gmail.com>
2013-10-13 21:58:52 +0900

    메모장만들기 소스 카피
    https://wikidocs.net/36 의 메모장 만들기 실습

---
+++ 프로그램제작실습/realtest.py
@@ -0,0 +1,23 @@
+import sys
+import time
+ 
+ def usange():
+ 	print("""
+Usage
+=====
+python %S -v : View memo
+python %s -a : Add memo
+""" % (sys.argv[0], sys.argv[0]))
+
+if not sys.argv[1:] or sys.argv[1] not in ['-v','-a']:
+	usage()
+elif sys.argv[1] == '-v':
+	try: print(open("memo.txt").read())
+	except IOError: print("memo does not exist!")
+elif sys.argv[1] == '-a':
+	word = input("Enter memo: ")
+	f = open("memo.txt", 'a')
+	f.write(time.ctime()+': '+word+'\n')
+	f.close()
+	print("Added")
+	
